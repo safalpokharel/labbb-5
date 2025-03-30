34fc2133f20ecc1121f06c781291af5e6be445ed
Safal Pokharel <safalpokharel@safals-air.mynetworksettings.com>
2025-03-29 20:47:39 -0400

    first commit

--- index.js
+++ index.js
@@ -4,37 +4,48 @@ const nedb = require("nedb-promises"); // load nedb module
 const app = express(); // init app
 const db = nedb.create('myfile.jsonl'); // init db
 
+app.use(express.json());
 app.use(express.static('public')); // enable static routing
 
-// add your http routes here
-app.get('/search', (req, res) => {
-    try {
-        const query = JSON.parse(req.query.find)
-        db.find(query)
-        .then(docs => {
-            const results = docs.map(doc => JSON.stringify(doc, null, 2)).join("\n")
-            res.send(results)
-        })
-    } catch (error) {
-        res.send('Couldnot execute search')
-    }
-})
 
-app.get('/insert', (req, res) => {
-    try {
-        const doc = JSON.parse(req.query.doc)
-        db.insert(doc)
-        .then(doc => {
-            res.send( "inserted \n" + JSON.stringify(doc, null, 2))
-        })
-    } catch (error) {
-        res.send("Couldn't insert document")
-    }
-})
+app.post('/data', (req, res) => {
+    db.insert(req.body)
+    .then(doc => res.send({_id: doc._id}))
+    .catch(error => res.status(500).send('Could not insert document: ' + error));
+});
 
-// . . . . .
-// default route
-app.all('*',(req,res)=>{res.status(404).send('Invalid URL.')});
 
-// start server
-app.listen( 3000, () => console.log('server started at http://localhost:3000') );
+app.get('/data', (req, res) => {
+    db.find({})
+    .then(docs => res.send(docs))
+    .catch(error => res.status(500).send("No documents found: " + error));
+});
+
+
+app.get('/data/:id', (req, res) => {
+    db.findOne({ _id: req.params.id })
+    .then(doc => res.send(doc))
+    .catch(error => res.status(500).send("Could not find document: " + error));
+});
+
+
+app.patch('/data/:id', (req, res) => {
+    db.updateOne({ _id: req.params.id }, { $set: req.body })
+    .then(res.send({ message: "Document updated successfully.", updatedData: req.body }))
+    .catch(error => res.status(500).send("Could not update document: " + error));
+});
+
+
+app.delete('/data/:id', (req, res) => {
+    db.deleteOne({ _id: req.params.id })
+    .then(result => res.send(result))
+    .catch(error => res.status(500).send("Could not delete document: " + error));
+});
+
+
+app.all('*', (req, res) => { 
+    res.status(404).send('Invalid URL.'); 
+});
+
+
+app.listen(3000, () => console.log('Server started at http://localhost:3000'));
--- public/script.js
+++ public/script.js
@@ -1,22 +1,59 @@
 const searchField = document.getElementById('search-field')
 const insertField = document.getElementById('insert-field')
 const searchButton = document.getElementById('search-button')
-const insertButton = document.getElementById('insert-button')
-const resultDiv = document.getElementById('result')
+const createButton = document.getElementById('create-button')
+const readButton = document.getElementById('read-button')
+const readAllButton = document.getElementById('read-all-button')
+const updateButton = document.getElementById('update-button')
+const deleteButton = document.getElementById('delete-button')
 
-searchButton.addEventListener('click', () => {
-    fetch('/search?find=' + searchField.value)
-    .then(res => res.text())
-    .then(txt => {
-        resultDiv.innerText = txt
+const resultDiv = document.getElementById('result')
+const apiUrl = "http://localhost:3000/data";
+
+createButton.addEventListener("click", async()=>{
+    const data = insertField.value;
+    const response = await fetch(apiUrl, {
+        method: "POST",
+        headers: {"Content-type": "application/json"},
+        body: data
     })
+    const result = await response.json()
+    resultDiv.innerText = "Created:" + JSON.stringify(result)
+
 })
 
-insertButton.addEventListener('click', () => {
-    fetch('/insert?doc=' + insertField.value)
-    .then(res => res.text())
-    .then(txt => {
-        resultDiv.innerText = txt
+readAllButton.addEventListener("click", async() =>{
+    const response = await fetch(apiUrl);
+    const result = await response.json();
+    resultDiv.innerText = JSON.stringify(result, null, 2);
+})
+
+readButton.addEventListener("click", async()=>{
+    const id = searchField.value;
+    const response = await fetch(`${apiUrl}/${id}`);
+    const result = await response.json();
+    resultDiv.innerText = "FOUND :" + JSON.stringify(result, null, 2);
+});
+
+updateButton.addEventListener("click", async()=>{
+    const id = searchField.value;
+    const data = insertField.value;
+
+    const response = await fetch(`${apiUrl}/${id}`,{
+        method: "PATCH",
+        headers: {"Content-type": "application/json"},
+        body: data
     })
+    const result = await response.json();
+    resultDiv.innerText = "UPDATED:" + JSON.stringify(result);
+})
+
+deleteButton.addEventListener("click", async()=>{
+    const id = searchField.value;
+    const response = await fetch(`${apiUrl}/${id}`,{
+        method: "DELETE"
+    });
+    const result = response.json()
+    resultDiv.innerText = "DELETED" + JSON.stringify(result)
 })
 
